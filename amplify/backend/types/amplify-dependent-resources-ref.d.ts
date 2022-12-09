b480e1991afc8228e36ffaa4302378177d82689e
kazuma-fujita <kazuma.fujita.zuma@gmail.com>
2022-12-09 16:45:43 +0900

    add Auth and Predictions categories

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,20 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "nextjsamplifytexttot0f22a82b": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "predictions": {
+        "translateText8e6b323a": {
+            "region": "string",
+            "sourceLang": "string",
+            "targetLang": "string"
+        }
+    }
+}
